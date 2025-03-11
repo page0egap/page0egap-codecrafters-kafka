f07d676e173bdefa95bd45c67f7746964eee5a75
ZHUPENGXU <zhupengxu06@qq.com>
2025-03-12 00:21:55 +0800

    concurrent requests

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
 #![allow(unused_imports)]
 use std::{
     io::{Read, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
+    thread::{self, JoinHandle},
 };
 
 use codecrafters_kafka::{
     request::{self, body::KafkaRequestBody, error::RequestError, KafkaRequest},
     response::{KafkaResponse, KafkaResponseHeader},
 };
+
+fn handle_stream(mut stream: TcpStream) {
+    println!("accepted new connection");
+    loop {
+        let request = KafkaRequest::try_from_reader(&mut stream);
+        let request = match request {
+            Ok(r) => r,
+            Err(_e) => {
+                dbg!("stream is eof");
+                break;
+            }
+        };
+        println!("recieve new request");
+        // generate response
+        let response = KafkaResponse::from_request(&request);
+        let response_bytes: Vec<u8> = response.into();
+        stream.write_all(&response_bytes).unwrap();
+        println!("response to new request");
+    }
+
+    println!("close the connection");
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -16,33 +39,22 @@ fn main() {
     // Uncomment this block to pass the first stage
     //
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
-    //
+
+    let mut threads = Vec::new();
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                loop {
-                    let request = KafkaRequest::try_from_reader(&mut stream);
-                    let request = match request {
-                        Ok(r) => r,
-                        Err(_e) => {
-                            dbg!("stream is eof");
-                            break;
-                        }
-                    };
-                    println!("recieve new request");
-                    // generate response
-                    let response = KafkaResponse::from_request(&request);
-                    let response_bytes: Vec<u8> = response.into();
-                    stream.write_all(&response_bytes).unwrap();
-                    println!("response to new request");
-                }
-
-                println!("close the connection");
+            Ok(stream) => {
+                threads.push(thread::spawn(|| {
+                    handle_stream(stream);
+                }));
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+    // clear all threads
+    for thread in threads {
+        thread.join().unwrap()
     }
 }
