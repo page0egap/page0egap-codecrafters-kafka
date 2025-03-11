2094dfa5e30e2b1993005e2d1c04b0afe673d4d5
ZHUPENGXU <zhupengxu06@qq.com>
2025-03-11 23:24:45 +0800

    handle api versions request

--- src/response/response_body/api_versions.rs
+++ src/response/response_body/api_versions.rs
@@ -97,7 +97,7 @@ impl KafkaResponseBodyApiVersions {
         // };
         let error_code = KafkaError::None;
         let api_keys = vec![Default::default()];
-        let throttle_time_ms = 0;
+        let throttle_time_ms = 1;
         match api_version {
             SupportApiVersionsRequestVersion::V0 => Self::V0(ApiVersionsResponseBodyV0 {
                 error_code,
