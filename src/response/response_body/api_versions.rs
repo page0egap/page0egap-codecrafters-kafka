46ad9f6159d24bb23ac48d793ee5ba247949f3b5
ZHUPENGXU <zhupengxu06@qq.com>
2025-03-11 23:08:10 +0800

    handle api versions request

--- src/response/response_body/api_versions.rs
+++ src/response/response_body/api_versions.rs
@@ -1,3 +1,5 @@
+use integer_encoding::VarInt;
+
 use crate::{
     consts::api_versions::{
         SupportApiVersionsRequestVersion, API_VERSIONS_API_KEY, API_VERSIONS_MAX_VERSION,
@@ -16,12 +18,12 @@ pub enum KafkaResponseBodyApiVersions {
 
 pub struct ApiVersionsResponseBodyV0 {
     error_code: KafkaError,
-    api_keys: ApiKeyRange,
+    api_keys: Vec<ApiKeyRange>,
 }
 
 pub struct ApiVersionsResponseBodyV1 {
     error_code: KafkaError,
-    api_keys: ApiKeyRange,
+    api_keys: Vec<ApiKeyRange>,
     /// The duration in milliseconds for which the request was throttled due to
     /// a quota violation, or zero if the request did not violate any quota.
     throttle_time_ms: i32,
@@ -29,7 +31,7 @@ pub struct ApiVersionsResponseBodyV1 {
 
 pub struct ApiVersionsResponseBodyV2 {
     error_code: KafkaError,
-    api_keys: ApiKeyRange,
+    api_keys: Vec<ApiKeyRange>,
     /// The duration in milliseconds for which the request was throttled due to
     /// a quota violation, or zero if the request did not violate any quota.
     throttle_time_ms: i32,
@@ -37,7 +39,7 @@ pub struct ApiVersionsResponseBodyV2 {
 
 pub struct ApiVersionsResponseBodyV3 {
     error_code: KafkaError,
-    api_keys: ApiKeyRange,
+    api_keys: Vec<ApiKeyRange>,
     /// The duration in milliseconds for which the request was throttled due to
     /// a quota violation, or zero if the request did not violate any quota.
     throttle_time_ms: i32,
@@ -45,7 +47,7 @@ pub struct ApiVersionsResponseBodyV3 {
 
 pub struct ApiVersionsResponseBodyV4 {
     error_code: KafkaError,
-    api_keys: ApiKeyRange,
+    api_keys: Vec<ApiKeyRange>,
     /// The duration in milliseconds for which the request was throttled due to
     /// a quota violation, or zero if the request did not violate any quota.
     throttle_time_ms: i32,
@@ -67,6 +69,18 @@ impl Default for ApiKeyRange {
     }
 }
 
+impl Into<Vec<u8>> for ApiKeyRange {
+    #[inline]
+    fn into(self) -> Vec<u8> {
+        self.api_key
+            .to_be_bytes()
+            .into_iter()
+            .chain(self.min_version.to_be_bytes())
+            .chain(self.max_version.to_be_bytes())
+            .collect()
+    }
+}
+
 impl KafkaResponseBodyApiVersions {
     pub fn error() -> Self {
         Self::V0(ApiVersionsResponseBodyV0 {
@@ -74,7 +88,7 @@ impl KafkaResponseBodyApiVersions {
             api_keys: Default::default(),
         })
     }
-    
+
     pub fn new(api_version: SupportApiVersionsRequestVersion) -> Self {
         // let error_code = if is_in_support_version(api_version) {
         //     KafkaError::None
@@ -131,12 +145,19 @@ impl Into<Vec<u8>> for ApiVersionsResponseBodyV0 {
     fn into(self) -> Vec<u8> {
         let error_code: i16 = self.error_code.into();
         let api_keys = self.api_keys;
+        let api_keys_length = api_keys.len();
+        let api_key_iter = api_keys
+            .into_iter()
+            .map(|api_key| {
+                let data: Vec<u8> = api_key.into();
+                data
+            })
+            .flatten();
         error_code
             .to_be_bytes()
             .into_iter()
-            .chain(api_keys.api_key.to_be_bytes())
-            .chain(api_keys.min_version.to_be_bytes())
-            .chain(api_keys.max_version.to_be_bytes())
+            .chain(api_keys_length.encode_var_vec())
+            .chain(api_key_iter)
             .collect()
     }
 }
@@ -146,13 +167,21 @@ impl Into<Vec<u8>> for ApiVersionsResponseBodyV1 {
     fn into(self) -> Vec<u8> {
         let error_code: i16 = self.error_code.into();
         let api_keys = self.api_keys;
+        let api_keys_length = api_keys.len();
         let throttle_time_ms = self.throttle_time_ms;
+
+        let api_key_iter = api_keys
+            .into_iter()
+            .map(|api_key| {
+                let data: Vec<u8> = api_key.into();
+                data
+            })
+            .flatten();
         error_code
             .to_be_bytes()
             .into_iter()
-            .chain(api_keys.api_key.to_be_bytes())
-            .chain(api_keys.min_version.to_be_bytes())
-            .chain(api_keys.max_version.to_be_bytes())
+            .chain(api_keys_length.encode_var_vec())
+            .chain(api_key_iter)
             .chain(throttle_time_ms.to_be_bytes())
             .collect()
     }
@@ -163,13 +192,21 @@ impl Into<Vec<u8>> for ApiVersionsResponseBodyV2 {
     fn into(self) -> Vec<u8> {
         let error_code: i16 = self.error_code.into();
         let api_keys = self.api_keys;
+        let api_keys_length = api_keys.len();
         let throttle_time_ms = self.throttle_time_ms;
+
+        let api_key_iter = api_keys
+            .into_iter()
+            .map(|api_key| {
+                let data: Vec<u8> = api_key.into();
+                data
+            })
+            .flatten();
         error_code
             .to_be_bytes()
             .into_iter()
-            .chain(api_keys.api_key.to_be_bytes())
-            .chain(api_keys.min_version.to_be_bytes())
-            .chain(api_keys.max_version.to_be_bytes())
+            .chain(api_keys_length.encode_var_vec())
+            .chain(api_key_iter)
             .chain(throttle_time_ms.to_be_bytes())
             .collect()
     }
@@ -180,15 +217,23 @@ impl Into<Vec<u8>> for ApiVersionsResponseBodyV3 {
     fn into(self) -> Vec<u8> {
         let error_code: i16 = self.error_code.into();
         let api_keys = self.api_keys;
+        let api_keys_length = api_keys.len();
         let throttle_time_ms = self.throttle_time_ms;
         let empty_tagged_fields = Vec::new();
+
+        let api_key_iter = api_keys
+            .into_iter()
+            .map(|api_key| {
+                let data: Vec<u8> = api_key.into();
+                data.into_iter()
+                    .chain(response::utils::tagged_fields_to_vec(&empty_tagged_fields))
+            })
+            .flatten();
         error_code
             .to_be_bytes()
             .into_iter()
-            .chain(api_keys.api_key.to_be_bytes())
-            .chain(api_keys.min_version.to_be_bytes())
-            .chain(api_keys.max_version.to_be_bytes())
-            .chain(response::utils::tagged_fields_to_vec(&empty_tagged_fields))
+            .chain(api_keys_length.encode_var_vec())
+            .chain(api_key_iter)
             .chain(throttle_time_ms.to_be_bytes())
             .chain(response::utils::tagged_fields_to_vec(&empty_tagged_fields))
             .collect()
@@ -200,15 +245,23 @@ impl Into<Vec<u8>> for ApiVersionsResponseBodyV4 {
     fn into(self) -> Vec<u8> {
         let error_code: i16 = self.error_code.into();
         let api_keys = self.api_keys;
+        let api_keys_length = api_keys.len();
         let throttle_time_ms = self.throttle_time_ms;
         let empty_tagged_fields = Vec::new();
+
+        let api_key_iter = api_keys
+            .into_iter()
+            .map(|api_key| {
+                let data: Vec<u8> = api_key.into();
+                data.into_iter()
+                    .chain(response::utils::tagged_fields_to_vec(&empty_tagged_fields))
+            })
+            .flatten();
         error_code
             .to_be_bytes()
             .into_iter()
-            .chain(api_keys.api_key.to_be_bytes())
-            .chain(api_keys.min_version.to_be_bytes())
-            .chain(api_keys.max_version.to_be_bytes())
-            .chain(response::utils::tagged_fields_to_vec(&empty_tagged_fields))
+            .chain(api_keys_length.encode_var_vec())
+            .chain(api_key_iter)
             .chain(throttle_time_ms.to_be_bytes())
             .chain(response::utils::tagged_fields_to_vec(&empty_tagged_fields))
             .collect()
